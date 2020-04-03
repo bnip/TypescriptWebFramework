c03bdf243804b71da5536327e3eaf6747ae23500
Bryan Nip <bryannip@buffalo.edu>
2020-04-03 14:14:10 -0400

    Passthrough methods

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,11 +13,25 @@ export interface UserProps {
 const rooturl = 'http://localhost:3000/users';
 
 export class User {
-  public eventing: Eventing = new Eventing();
+  public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rooturl);
   public attributes: Attributes<UserProps>;
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  // We are not trying to call a function right here.
+  // Instead we are trying to return a reference to the events.on() method
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
   }
 }
