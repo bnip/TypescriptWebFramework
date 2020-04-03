abc5e5e87586f2f792bddb75e45d640b1808aee2
Bryan Nip <bryannip@buffalo.edu>
2020-04-03 15:06:16 -0400

    Extending the User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
-import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
+import { Model } from './Model';
 
 export interface UserProps {
   // By adding question marks we make the properties optional.
@@ -13,54 +10,4 @@ export interface UserProps {
 
 const rooturl = 'http://localhost:3000/users';
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rooturl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  // We are not trying to call a function right here.
-  // Instead we are trying to return a reference to the events.on() method
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((reponse: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
-  }
-}
+export class User extends Model<UserProps> {}
