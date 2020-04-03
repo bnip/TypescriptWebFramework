2ad549275d79ab7f0ccf735d1492079fbeec1fc9
Bryan Nip <bryannip@buffalo.edu>
2020-04-03 11:26:18 -0400

    - Added REST and Fetch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({ name: 'myName', age: 20 });
+// const user = new User({ name: 'myName', age: 20 });
 
-user.set({ name: 'newName', age: 999 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+// user.on('change', () => {
+//   console.log('Change #1');
+// });
+// user.on('change', () => {
+//   console.log('Change #2');
+// });
+
+// console.log(user);
+
+import axios from 'axios';
+
+axios.post('http://localhost:3000/users', {
+  name: 'myname',
+  age: 20,
+});
+
+axios.get('http://localhost:3000/users/1');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,22 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
   // By adding question marks we make the properties optional.
   // This gives us the flexiblity to update only one property if we wanted to
+  id?: number;
   name?: string;
   age?: number;
 }
 
+// Type Alias
+// Empty function, function that has no arguments or return values
+type Callback = () => void;
+
 export class User {
+  // We don't know the names of the events being passed in
+  // So we use [key: string] to tell typescript that the keys are strings
+  // And the keys will point to values that are an array of Callback functions
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -15,5 +26,35 @@ export class User {
   set(update: UserProps): void {
     // Overwrites this.data with the passed in data of type UserProps
     Object.assign(this.data, update);
+  }
+
+  // () => void as a prop tells typescript that the passed in parameter is a function
+  on(eventName: string, callback: Callback) {
+    // This will fall back on [] if we get undefined
+    // This guarantees handlers will be an array
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    // Assign array back to event in events
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  fetxh(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((reponse: AxiosResponse): void => {
+        this.set(reponse.data);
+      });
   }
 }
